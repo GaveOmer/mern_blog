19d1de4569a8ab2c53167516ac195ebf6e48f6dd
feitan <ilya.shkoda06@gmail.com>
2024-02-28 14:28:41 +0300

    backend part is completed(crud, static, need some refactoring

--- backend/index.js
+++ backend/index.js
@@ -1,122 +1,85 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import mongoose from 'mongoose';
 import cors from 'cors';
-import { validationResult } from 'express-validator';
-
-import { registerValidation } from './validations/auth.js';
-
-import UserModel from './models/User.model.js';
-
-import checkAuth from './utils/checkAuth.js';
+import express from 'express';
+import mongoose from 'mongoose';
+import multer from 'multer';
+import fs from 'fs';
+import { handleValidationErrors, checkAuth } from './utils/index.js';
+import {
+	registerValidation,
+	loginValidation,
+	postCreateValidation,
+} from './validations.js';
+import { UserController, PostController } from './controllers/index.js';
 /////////////////////////////////
 const app = express();
+//multer/////////////////////////
+const dir = './uploads';
+if (!fs.existsSync(dir)) {
+	fs.mkdirSync(dir, { recursive: true });
+}
+const storage = multer.diskStorage({
+	destination: (_, __, cb) => {
+		cb(null, 'uploads');
+	},
+	filename: (_, file, cb) => {
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ storage });
 // middlewares //////////////////
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-	cors({
-		origin: 'http://localhost:5173',
-		methods: ['POST', 'PUT', 'GET', 'DELETE'],
-	})
-);
+app.use('/uploads', express.static('uploads'));
+// app.use(
+// 	cors({
+// 		origin: 'http://localhost:5173',
+// 		methods: ['POST', 'PUT', 'GET', 'DELETE'],
+// 	})
+// );
 ////////////////////////////////
 app.get('/', (req, res) => {
 	res.send('Welcome');
 });
 
-app.post('/auth/login', async (req, res) => {
-	try {
-		const user = await UserModel.findOne({ email: req.body.email });
-		if (!user) {
-			return res.status(404).json({
-				message: 'Не авторизоваться',
-			});
-		}
+app.post(
+	'/auth/login',
+	loginValidation,
+	handleValidationErrors,
+	UserController.login
+);
+app.post(
+	'/auth/register',
+	registerValidation,
+	handleValidationErrors,
+	UserController.register
+);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
-		const isValidPass = await bcrypt.compare(
-			req.body.password,
-			user._doc.passwordHash
-		);
-		if (!isValidPass) {
-			return res.status(404).json({
-				message: 'Неверный логин или пароль',
-			});
-		}
-
-		const token = jwt.sign(
-			{
-				_id: user._id,
-			},
-			'secret123',
-			{
-				expiresIn: '30d',
-			}
-		);
-
-		const { passwordHash, ...userData } = user._doc;
-		res.json({
-			...userData,
-			token,
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({
-			message: 'Не удалось авторизоваться',
-		});
-	}
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+	res.json({
+		url: `uploads/${req.file.originalname}`,
+	});
 });
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-	try {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json(errors.array());
-		}
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post(
+	'/posts',
+	checkAuth,
+	postCreateValidation,
+	handleValidationErrors,
+	PostController.create
+);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+	'/posts/:id',
+	checkAuth,
 
-		const password = req.body.password;
-		const salt = await bcrypt.genSalt(10);
-		const hash = await bcrypt.hash(password, salt);
-		const doc = new UserModel({
-			email: req.body.email,
-			fullName: req.body.fullName,
-			avatarUrl: req.body.avatarUrl,
-			passwordHash: hash,
-		});
-		const user = await doc.save();
-
-		const token = jwt.sign(
-			{
-				_id: user._id,
-			},
-			'secret123',
-			{
-				expiresIn: '30d',
-			}
-		);
-
-		const { passwordHash, ...userData } = user._doc;
-
-		res.json({
-			...userData,
-			token,
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({
-			message: 'Не удалось зарегистрироваться',
-		});
-	}
-});
-
-app.get('/auth/me', checkAuth, (req, res) => {
-	try {
-		res.json({
-			success: true,
-		});
-	} catch (err) {}
-});
+	postCreateValidation,
+	handleValidationErrors,
+	PostController.update
+);
 
 mongoose
 	.connect(
@@ -124,8 +87,8 @@ mongoose
 	)
 	.then(() => {
 		console.log('connected');
-		app.listen('3000', () => {
-			console.log('listening on http://localhost:3000');
+		app.listen('5000', () => {
+			console.log('listening on http://localhost:5000');
 		});
 	})
 	.catch((error) => {
